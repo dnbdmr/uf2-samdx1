0c99ba2663999bf9ec699446f44c228c87ac649b
dnbdmr <dnbdmr@gmail.com>
2020-06-25 05:11:05 -0700

    Added board 67keys

---
+++ boards/67keys/board_config.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "DNBDMR"
+#define PRODUCT_NAME "67 Keyboard with Trackpoint"
+#define VOLUME_LABEL "KEYBOOT"
+#define INDEX_URL ""
+#define BOARD_ID "SAMD21E18A-67keys-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001E
+
+#define LED_PIN PIN_PA22
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+#define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
+#define BOARD_RGBLED_DATA_PIN             PIN_PA00
+
+#endif
